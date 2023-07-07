87559e63e4e25f9351fdcba515678529cb0c2c8b
“Sam Manalo” <sam.manalo@supportcommunity.com>
2023-07-08 01:30:08 +0800

    Email Design

--- Server/index.js
+++ Server/index.js
@@ -22,19 +22,46 @@ app.post('/', (req, res) => {
         return string.charAt(0).toUpperCase() + string.slice(1);
     };
 
+    const currentYear = new Date().getFullYear(); // Get the current year
+
     const mailOptions = {
         from: process.env.FROM_EMAIL,
         to: process.env.TO_EMAIL,
         subject: capitalizeFirstLetter(req.body.subject),
         html: `
-            <div style="border: 1px solid #ccc; padding: 10px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);">
-      <div>
-        <p>From: ${req.body.name}<br>Email: ${req.body.email}<br>Contact: ${req.body.phoneNumber}</p>
-        <p>${req.body.message}</p>
-        <img src="https://github.com/Antholem/React-Portfolio/blob/main/App/logo.png?raw=true" alt="Image" style="max-width: 100%; height: auto;">
+      <div style="display: flex; justify-content: center;">
+        <div style="padding: 15px; width: 100%;">
+          <div style="border-bottom: 2px solid #f3f2f1; padding-bottom: 5px;">
+            <img src="https://github.com/Antholem/React-Portfolio/blob/main/App/logo.png?raw=true" alt="Image" style="width: 50px; height: auto;" />
+          </div>
+          <div style="padding-top: 15px; padding-bottom: 15px;">
+            <div style="padding-top: 5px; display: flex; align-items: center;">
+              <img src="https://github.com/Antholem/React-Portfolio/blob/main/App/src/assets/images/email-icons/user.png?raw=true" alt="Phone Icon" style="width: 15px; height: auto; margin-right: 5px;" />
+              <p style="margin: 0;">${req.body.name}</p>
+            </div>
+            <div style="padding-top: 5px; display: flex; align-items: center;">
+              <img src="https://github.com/Antholem/React-Portfolio/blob/main/App/src/assets/images/email-icons/mail.png?raw=true" alt="Email Icon" style="width: 15px; margin-right: 5px;" />
+              <p style="margin: 0;">${req.body.email}</p>
+            </div>
+            <div style="padding-top: 5px; display: flex; align-items: center;">
+              <img src="https://github.com/Antholem/React-Portfolio/blob/main/App/src/assets/images/email-icons/number.png?raw=true" alt="Phone Icon" style="width: 15px; height: auto; margin-right: 5px;" />
+              <p style="margin: 0;">${req.body.phoneNumber}</p>
+            </div>
+            <div style="padding-top: 20px;">
+              <p>${req.body.message}</p>
+            </div>
+          </div>
+          <div style="border-top: 2px solid #f3f2f1;">
+            <p style="color: #454545; font-size: 10px;">
+              &copy; ${currentYear} Sam Manalo
+            </p>
+            <p style="color: #454545; font-size: 10px;">
+              Mabalacat City, Pampanga, Philippines
+            </p>
+          </div>
+        </div>
       </div>
-    </div>
-        `,
+    `,
     };
 
     transporter.sendMail(mailOptions, (error, info) => {
