b205f3de22f3684bc32ae0faf30cbcc86b02fbda
“Sam Manalo” <sam.manalo@supportcommunity.com>
2023-07-09 00:02:35 +0800

    Animate Pulse Hero Border

--- App/src/components/banner/DevImg.jsx
+++ App/src/components/banner/DevImg.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { BannerImg } from '../../assets/images';
+import { motion } from 'framer-motion';
 
 const RightBanner = () => {
   return (
     <div className='w-full lgl:w-1/2 flex justify-center items-center relative'>
       <img
-        className='w-[300px] h-[400px] lgl:w-[500px] lgl:h-[680px] z-10'
+        className='w-[300px] h-[400px] lgl:w-[500px] lgl:h-[680px] z-10 py-1'
         src={BannerImg}
         alt='bannerImg'
       />
-      <div className='absolute bottom-0 w-[350px] h-[400px] lgl:w-[500px] lgl:h-[680px] bg-gradient-to-r from-[#1e2024] to-[#202327] shadow-shadowOne flex justify-center items-center'></div>
+      <motion.div
+        className='absolute bottom-0 w-[350px] h-[400px] lgl:w-[500px] lgl:h-[680px] bg-gradient-to-r rounded-lg border-2 border-designColor shadow-shadowOne flex justify-center items-center animate-pulse'
+        transition={{ duration: 0.3 }}
+      ></motion.div>
     </div>
-  )
-}
+  );
+};
 
 export default RightBanner;
